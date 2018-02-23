2f4b65fe231dbccd14715a36798ffacf00c567c1
sduemler <samduemler@gmail.com>
2018-02-23 13:12:51 -0500

    added classes

--- Documents/College Classes/CSE 321/src/Calc.java
+++ Documents/College Classes/CSE 321/src/Calc.java
@@ -5,20 +5,20 @@
 
 public class Calc
 {
-   static public int add (int a, int b)
+   //static public int add (int a, int b)
    {
       return a + b;
    }
    
-   static public int subtract (int a, int b){
+   //static public int subtract (int a, int b){
 	   return a - b;
    }
    
-   static public int multiply (int a, int b){
+   //static public int multiply (int a, int b){
 	   return a * b;
    }
    
-   static public int divide(int a, int b)
+   //static public int divide(int a, int b)
    {
 	   return a/b;
    }
